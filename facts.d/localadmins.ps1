716489d4ad0534c830cbfbe7356f32886047f062
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2022-02-11 21:05:38 +1100

    Delete facts.d directory

--- facts.d/localadmins.ps1
+++
@@ -1,5 +0,0 @@
-$ComputerName=hostname
-$AdminUsers=Get-LocalGroupMember Administrators | select Name
-$localadmins=(($AdminUsers -replace "\@{Name=$ComputerName\\", "")-replace '\}',',')
-$strfact=Write-output "localadmins=$localadmins"
-Write-output $strfact.TrimEnd(",") 
